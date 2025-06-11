58acd9a110e04c4eb399cfd5c86c3a0ec849967f
Hebel15Bir <200211303+Hebel15Bir@users.noreply.github.com>
2025-06-11 14:46:16 +0300

    Update actions.js

--- src/server/actions.js
+++ src/server/actions.js
@@ -5,85 +5,96 @@ import { connectToDatabase, disconnectDatabase } from './db';
 import { Registrant } from './models';
 import { todayEthCalendar } from '../other/util';
 import fs from 'fs/promises';
+import { spawn } from 'child_process';
 
 cloudinary.config({
-  cloud_name: 'dxmwtld0d',
-  api_key: '143241854443167',
-  api_secret: 'E0PKUYkmSHhaQqHfPKi-YMUfw0o',
+	cloud_name: 'dxmwtld0d',
+	api_key: '143241854443167',
+	api_secret: 'E0PKUYkmSHhaQqHfPKi-YMUfw0o',
 });
 
 export async function registerStudent(studentData) {
-  const file = studentData.get('file');
-  if (!file) {
-    return { msgType: 'red', message: 'እባክዎ የተማሪ ምስል ያስገቡ።' };
-  }
-  if (!file.size) {
-    return { msgType: 'red', message: 'እባክዎ የተማሪ ምስል ያስገቡ።' };
-  }
+	const file = studentData.get('file');
+	if (!file) {
+		return {
+			success: '',
+			error: 'እባክዎ የተማሪ ምስል ያስገቡ።',
+		};
+	}
+	if (!file.size) {
+		return {
+			success: '',
+			error: 'እባክዎ የተማሪ ምስል ያስገቡ።',
+		};
+	}
 
-  let photoUrl;
+	let photoUrl;
 
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
+	const arrayBuffer = await file.arrayBuffer();
+	const buffer = Buffer.from(arrayBuffer);
 
-  try {
-    photoUrl = await new Promise((resolve, reject) => {
-      cloudinary.uploader
-        .upload_stream({}, (error, result) => {
-          if (error) reject(error);
-          else resolve(result.secure_url);
-        })
-        .end(buffer);
-    });
-  } catch {
-    return {
-      msgType: 'red',
-      message: 'ምስሉ በአግባቡ አልተጫነም። እባክዎ እንደገና ይሞክሩ።',
-    };
-  }
+	try {
+		photoUrl = await new Promise((resolve, reject) => {
+			cloudinary.uploader
+				.upload_stream({}, (error, result) => {
+					if (error) reject(error);
+					else resolve(result.secure_url);
+				})
+				.end(buffer);
+		});
+	} catch {
+		return {
+			success: '',
+			error: 'ምስሉ በአግባቡ አልተጫነም። እባክዎ እንደገና ይሞክሩ።',
+		};
+	}
 
-  await connectToDatabase();
-  const registrant = new Registrant({
-    fullName: studentData.get('fullName'),
-    kName: studentData.get('kName'),
-    age: +studentData.get('age'),
-    photoUrl: photoUrl,
-    sex: studentData.get('sex'),
-    phoneNo: studentData.get('phoneNo'),
-    isOwn: studentData.get('isOwn') === 'on',
-    ownerName: studentData.get('ownerName'),
-    priesthood: studentData.get('priesthood'),
-    isNewStudent: studentData.get('isNewStudent') === 'on',
-    registryDate:
-      studentData.get('isNewStudent') === 'on'
-        ? todayEthCalendar()
-        : {
-            // {
-            year: +studentData.get('year'),
-            month: studentData.get('month'),
-            date: +studentData.get('date'),
-          },
-    classDetails: {
-      classroom: studentData.get('classroom'),
-      subject: studentData.get('subject'),
-      classTime: studentData.get('classTime'),
-    },
-    confirmStatus: 'registered',
-    paymentId: studentData.get('paymentId'),
-  });
+	await connectToDatabase();
+	const registrant = new Registrant({
+		fullName: studentData.get('fullName'),
+		kName: studentData.get('kName'),
+		age: parseInt(studentData.get('age')),
+		photoUrl: photoUrl,
+		sex: studentData.get('sex'),
+		phoneNo: studentData.get('phoneNo'),
+		isOwn: studentData.get('isOwn') === 'on',
+		ownerName: studentData.get('ownerName'),
+		priesthood: studentData.get('priesthood'),
+		isNewStudent: studentData.get('isNewStudent') === 'on',
+		registryDate:
+			studentData.get('isNewStudent') === 'on'
+				? todayEthCalendar()
+				: {
+						year: parseInt(studentData.get('year')),
+						month: studentData.get('month'),
+						date: parseInt(studentData.get('date')),
+				  },
+		classDetails: {
+			classroom: studentData.get('classroom'),
+			subject: studentData.get('subject'),
+			classTime: studentData.get('classTime'),
+		},
+		confirmStatus: 'registered',
+		paymentId: studentData.get('paymentId'),
+	});
 
-  try {
-    await registrant.save().then(() => console.log('successful'));
-  } catch (err) {
-    console.log(err);
-    return { msgType: 'red', message: 'እባክዎ አስፈላጊዎቹን መረጃዎች በሙሉ ይሙሉ።' };
-  }
+	try {
+		await registrant.save();
+	} catch (err) {
+		console.log(todayEthCalendar());
+		console.log(registrant);
+		console.log(err);
+		return {
+			success: '',
+			error: 'እባክዎ አስፈላጊዎቹን መረጃዎች በሙሉ ይሙሉ።',
+		};
+	}
 
-  await disconnectDatabase();
+	await disconnectDatabase();
 
-  return {
-    msgType: 'green',
-    message:
-      'ምዝገባዎ በአግባቡ ተጠናቋል። የክፍያ ደረሰኝ በመያዝ ወደ ጉባኤ ቤቱ ጽሕፈት ቤት ሔደው ምዝገባዎን ያረጋግጡ።',
-  };
+	return {
+		success:
+			'ምዝገባዎ በአግባቡ ተጠናቋል። የክፍያ ደረሰኝ በመያዝ ወደ ጉባኤ ቤቱ ጽሕፈት ቤት ሔደው ምዝገባዎን ያረጋግጡ።',
+		error: '',
+	};
 }
